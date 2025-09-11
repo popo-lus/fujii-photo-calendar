64a5f1309052ec7e9736a4f42a5296d3775b322c
popo-lus <awayuki_lsa@outlook.jp>
2025-09-11 19:54:24 +0900

    Revert "Merge pull request #2 from popo-lus/001-features-main-page"
    This reverts commit e393f0acaff300229e2c6d3269b10b87d69136fd, reversing
    changes made to c339d6659db4201f9cdb537ade32ba5d83d6fb46.

--- app/android/app/src/main/kotlin/com/example/fujii_photo_calendar/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fujii_photo_calendar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app/lib/presentation/screens/calendar/widgets/empty_month_placeholder.dart
+++
@@ -1,9 +0,0 @@
-// (T023) 空月プレースホルダー
-import 'package:flutter/material.dart';
-
-class EmptyMonthPlaceholder extends StatelessWidget {
-  const EmptyMonthPlaceholder({super.key});
-  @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('No photos yet'));
-}
